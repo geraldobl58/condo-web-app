41c4c96f74bd203b6c757c6c4ded936c731068c5
Geraldo Luiz <geraldobl58@gmail.com>
2023-10-25 17:57:28 -0300

    feat: Add currency component

---
+++ src/components/ui/currency.tsx
@@ -0,0 +1,24 @@
+import { formattedPrice } from "@/lib/utils";
+import { useEffect, useState } from "react";
+
+interface CurrencyProps {
+  value?: string | number;
+}
+
+const Currency = ({ value }: CurrencyProps) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <div className="font-semibold">{formattedPrice.format(Number(value))}</div>
+  );
+};
+
+export default Currency;
