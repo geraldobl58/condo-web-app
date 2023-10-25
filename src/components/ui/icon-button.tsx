dbcadf17a77c1c55d4b1e97cc35286e40b27eae9
Geraldo Luiz <geraldobl58@gmail.com>
2023-10-25 17:57:47 -0300

    feat: Add icon component

---
+++ src/components/ui/icon-button.tsx
@@ -0,0 +1,36 @@
+import { MouseEventHandler } from "react";
+
+import { cn } from "@/lib/utils";
+
+interface IconButtonProps {
+  onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
+  icon: React.ReactElement;
+  className?: string;
+}
+
+const IconButton = ({ onClick, className, icon }: IconButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={cn(
+        `
+        rounded-full
+        flex
+        items-center
+        justify-center
+        bg-white
+        border
+        shadow-md
+        p-2
+        hover:scale-110
+        transition
+      `,
+        className
+      )}
+    >
+      {icon}
+    </button>
+  );
+};
+
+export default IconButton;
